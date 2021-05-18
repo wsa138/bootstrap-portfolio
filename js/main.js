2b64c1dae6308ab4b336e90822c6f7341b8b6755
William Addesso <wsaddesso@gmail.com>
2021-05-18 02:38:31 -0400

    Edited and fixed some styling. Added links to images of projects in portfolio section.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $('#email-address').click(displayAddress);
 function displayAddress() {
   alert('wsaddesso@gmail.com');
 }
+
+$('.navbar a').click(function () {
+  console.log('#' + this.dataset.value);
+  $('body,html').animate(
+    {
+      scrollTop: $('.' + this.dataset.value).offset().top,
+    },
+    1000
+  );
+});
